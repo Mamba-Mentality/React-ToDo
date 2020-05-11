5ef026aeb94f6b7ad56e89fcb51c3cc0e45c9e33
Christopher Gatherer <u114018@financeofamerica.com>
2020-05-11 14:05:18 -0400

    ToDo Component

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var ToDoApp = React.createClass({
+    render: function () {
+        return (
+            <div>
+                ToDoApp is rendered.
+            </div>
+        )
+    }
+});
+
+module.exports = ToDoApp;
